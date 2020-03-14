5f3f5041698ef36e926ec025fd761d50ea20e56e
snibbels <12849528+snibbels@users.noreply.github.com>
2020-03-14 15:07:18 +0100

    update

---
+++ precache-manifest.9f31be64b06af5cddd7567fb34c6eda9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2efa137c4897fdcb7c61edf12666dae",
+    "url": "/tmu-planer-live/index.html"
+  },
+  {
+    "revision": "2f89e7acd322c7e47cf2",
+    "url": "/tmu-planer-live/static/css/2.823a9121.chunk.css"
+  },
+  {
+    "revision": "71c6aaae42b103972e9d",
+    "url": "/tmu-planer-live/static/css/main.223eda5e.chunk.css"
+  },
+  {
+    "revision": "2f89e7acd322c7e47cf2",
+    "url": "/tmu-planer-live/static/js/2.bc1d58d0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/tmu-planer-live/static/js/2.bc1d58d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71c6aaae42b103972e9d",
+    "url": "/tmu-planer-live/static/js/main.dab659aa.chunk.js"
+  },
+  {
+    "revision": "77c77dc7638e87d33108",
+    "url": "/tmu-planer-live/static/js/runtime-main.6f43d328.js"
+  }
+]);
