cee4aab86acf54807a7142c17119ab0a6d51816b
= <=>
2020-03-14 22:33:06 +0100

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-This is the live version of tmu-planer. The App is found under https://snibbels.github.io/tmu-planer-live.
---
+++ precache-manifest.9a3434c3513dd789cdf85aab724e1472.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d53700c310459fc9b881887d066df74",
+    "url": "/tmu-planer-live/index.html"
+  },
+  {
+    "revision": "ae9ecf18e8d9a330a0a9",
+    "url": "/tmu-planer-live/static/css/2.8fe25b83.chunk.css"
+  },
+  {
+    "revision": "20ce423745bdd4a499a5",
+    "url": "/tmu-planer-live/static/css/main.157a1032.chunk.css"
+  },
+  {
+    "revision": "ae9ecf18e8d9a330a0a9",
+    "url": "/tmu-planer-live/static/js/2.a9f5ba68.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/tmu-planer-live/static/js/2.a9f5ba68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20ce423745bdd4a499a5",
+    "url": "/tmu-planer-live/static/js/main.ddcebdf0.chunk.js"
+  },
+  {
+    "revision": "14873896b9ef416229b2",
+    "url": "/tmu-planer-live/static/js/runtime-main.f6d3cda9.js"
+  }
+]);
